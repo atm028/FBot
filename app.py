0292fb9a0670bc739096ffdfa6aa4982361885bc
atm028 <ituser@ubuntu-512mb-fra1-01.localdomain>
2016-12-10 23:07:42 +0000

    Issues fix and user name getting

--- app.py
+++ app.py
@@ -2,19 +2,22 @@ from flask import Flask, request
 import consul
 import requests
 import json
+import sys
 
 app = Flask(__name__)
+
 csl = consul.Consul()
 global TOKEN
 TOKEN = None
 
 MESSAGE_URL = "https://graph.facebook.com/v2.6/me/messages"
 SETTINGS_URL = "https://graph.facebook.com/v2.6/me/thread_settings"
+USER_URL = "https://graph.facebook.com/v2.6/"
 
 @app.route("/fhook", methods=["GET"])
 def verify_bot():
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        return request.args.get("hub.hallenge"], 200
+        return request.args.get["hub.hallenge"], 200
     return "OK", 200
 
 @app.route("/fhook", methods=["POST"])
@@ -24,22 +27,33 @@ def webhook():
         for evt in entry["messaging"]:
             try:
                 sender_id = evt["sender"]["id"]
-                recipient_id = evt["recipient"]["id"]
-                message = evt["message"]["text"]
-                handleMsg(sender_id, recipient_id, message)
+                
+                msg = None
+                if "message" in evt: message = evt["message"]["text"]
+                elif "postback" in evt: 
+                    params = {"access_token": TOKEN}
+                    r = requests.get(USER_URL+str(sender_id), params=params)
+                    if r.status_code == 200:
+                        r = r.json()
+                        user_name = r["first_name"] 
+                        message = "Hi "+str(user_name)+", what would you like to do tonight?"
+
+                if message is not None: handleMsg(sender_id, message)
                 return "OK", 200
-            except: 
+            except:
+                e = sys.exc_info()[0]
+                print(e) 
                 return "FAIL", 501
                 pass
     return "OK", 200
 
 #The handler will be bottleneck in case of heavy load. So it it should be point of scaling
 #For not heavy load should be OK
-def handleMsg(sid, rid, msg):
-    params = {"access_token", TOKEN}
+def handleMsg(sid, msg):
+    params = {"access_token": TOKEN}
     headers = {"Content-Type": "application/json"}
     data = json.dumps({
-        "recipient": {"id": rcp_id},
+        "recipient": {"id": sid},
         "message": {"text": msg}
     })
     try: r = requests.post(MESSAGE_URL, params=params, headers=headers, data=data)
@@ -69,7 +83,14 @@ def FBStartButton():
 def main():
     try:
         global TOKEN
-        k, v = csl.get("Facebook/Config/TOKEN")
-        TOKEN = v["Value"}
+        k, v = csl.kv.get("Facebook/Config/TOKEN")
+        #TOKEN = str(v["Value"])
+        TOKEN = "EAAY2o4XEu5kBAKaRGjZAbdN6UKgxjp3sEx97urMlhbWyOFxrZAQwNfL3CWRsCJnQSSmIqaIA6iKJKiqeGVBbXGXyZBS2LLpdvmXlQo4kWO8aZAUEm9No4O5mQF0admZCxslTeTIYlZCRzQn1mddhIS2BolsGU1Sjg3AaC6xx98IAZDZD"
+
+        FBWelcomeMessage()
+        FBStartButton()
         app.run(port=8000, host="127.0.0.1")
     except: pass
+
+
+if __name__ == "__main__": main()
